e97db7a7939a7d969507296bcef7b8fb9c25d927
asfand53710 <asfandb893@gmail.com>
2025-10-24 10:29:51 +0500

    did some changes in the code

--- form/src/components/clothes/Clothes.jsx
+++ form/src/components/clothes/Clothes.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Card } from "antd";
 import { PlusCircleFilled } from "@ant-design/icons";
-import data from '../clothes/ClothesData';
+import data from "../clothes/ClothesData";
 
 const { Meta } = Card;
 
@@ -15,99 +15,6 @@ function Clothes({
   useEffect(() => {
     console.log("Updated Product Data: ", cartItems);
   }, [cartItems]);
-
-  // const data = [
-  //   {
-  //     key: 1,
-  //     label: "Nike Clothes",
-  //     name: "Nike",
-  //     desc: "Made for all-day comfort, our PerfectStretch fabric has four-way stretch and a polished look. This button-down top puts it to good use, adding a zippered chest pocket and sweat-wicking support to get you through your daily schedule.",
-  //     image:
-  //       "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/5e6135e6-f7c9-47c7-9279-20ab2322fabe/M+NK+DF+24.7+PS+SS+BUTTON+DOWN.png",
-  //     alt: "Men's Dri-FIT Short-Sleeve Snap-Down Top",
-  //   },
-  //   {
-  //     key: 2,
-  //     label: "Adidas Clothes",
-  //     name: "Adidas",
-  //     desc: "Built on firm foundations. This Real Madrid home authentic jersey marks the Bernabéu's latest refurbishment with a design featuring subtle shapes, textures and colors borrowed from its long and storied history. Created to be worn at the legendary stadium in the 25/26 season, it fields a performance-focused construction for quick-fire soccer and a heat-applied club crest for unparalleled inspiration.",
-  //     image:
-  //       "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/2301d0c0b0db4c9f8779b07fb7e9127c_9366/Real_Madrid_25-26_Home_Authentic_Jersey_White_JV5918_HM1.jpg",
-  //     alt: "Real Madrid 25/26 Home Authentic Jersey",
-  //   },
-  //   {
-  //     key: 3,
-  //     label: "Puma Clothes",
-  //     name: "Puma",
-  //     desc: "Welcome to the House of Graphics. This collection transforms streetwear essentials like tees or hoodies with unique graphics inspired by sports. Part of the collection, this tee has a relaxed fit and a classic crew neck.",
-  //     image:
-  //       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/630248/01/mod04/fnd/PNA/fmt/png/House-Of-Graphics-What-About-Men's-Tee",
-  //     alt: "House Of Graphics What About Men's Tee",
-  //   },
-  //   {
-  //     key: 4,
-  //     label: "Outfitters Clothes",
-  //     name: "Outfitters",
-  //     desc: "Strut your stuff in these Slim Cropped Jeans that hit all the right notes! The perfect blend fabric hugs your curves while giving you room to groove. These sassy cropped beauties are ready to play nice with your favorite tops and make your casual outfits pop with personality. Who said comfort can't be cute?",
-  //     image:
-  //       "https://outfitters.com.pk/cdn/shop/files/F0556109629_2_copy.jpg?v=1750239245",
-  //     alt: "Slim Cropped Jeans",
-  //   },
-  //   {
-  //     key: 5,
-  //     label: "Nike Clothes",
-  //     name: "Nike",
-  //     desc: "We took our iconic smooth-on-both-sides fleece and delivered a heavyweight version with an even warmer, more luxurious feel. It's paired with a super-spacious fit and subtle Nike Tech details.",
-  //     image:
-  //       "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/a5105ee0-fdc0-4e05-9e10-27ff00d5b519/M+NK+TCH+FLC+HOODIE+DYE.png",
-  //     alt: "Nike Tech",
-  //   },
-  //   {
-  //     key: 6,
-  //     label: "Nike Clothes",
-  //     name: "Nike",
-  //     desc: "Go with the flow in these classic woven shorts. The above-the-knee fit feels relaxed through the seat and thighs for a casual fit.",
-  //     image:
-  //       "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/375bf9c9-d662-4c29-b47a-0eba3e821761/M+NK+CLUB+FLOW+WASH+SHORT.png",
-  //     alt: "Nike Club",
-  //   },
-  //   {
-  //     key: 7,
-  //     label: "Adidas Clothes",
-  //     name: "Adidas",
-  //     desc: "Sometimes history just gets it right. Taken straight from the adidas archives, this signature track jacket nails a timeless look. An embroidered Trefoil logo adds a touch of pizzazz, and the supremely soft cotton blend makes it perfect for everyday wear. With a slim fit and full zip, it gives any outfit a dash of attitude. Our favorite part: the stand-up collar for that tailored vibe. A minimum of 70% of this product is a blend of recycled and renewable materials.",
-  //     image:
-  //       "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/74889fc94d7646dc96cc8475e9c999af_9366/Adicolor_Classics_Beckenbauer_Track_Jacket_Green_JY1280_21_model.jpg",
-  //     alt: "Adicolor Classics Beckenbauer Track Jacket",
-  //   },
-  //   {
-  //     key: 8,
-  //     label: "Adidas Clothes",
-  //     name: "Adidas",
-  //     desc: "Hit the tennis court in a style you can call your own. Designed for the world's premier grass court tournament, this tee combines heritage adidas vibes with modern performance. It's built with CLIMACOOL+, our best cooling innovation. The slim fit keeps you quick and the FreeLift design makes room for dominant overhead play.",
-  //     image:
-  //       "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/d12af6047e4f4820bb452e32d87bb98c_9366/Tennis_Pro_Climacool_FreeLift_Tee_White_JM1161_25_model.jpg",
-  //     alt: "Tennis Pro Climacool+ FreeLift Tee",
-  //   },
-  //   {
-  //     key: 9,
-  //     label: "Puma Clothes",
-  //     name: "Puma",
-  //     desc: "MMQ = “Macht's mit Qualität.” Translated from German, it means “Made with Quality” and represents the finest in PUMA's designs and materials. Minimalistic aesthetics, contemporary silhouettes, with a passion for sophisticated details. These sweatpants come in a relaxed fit and are made with a sturdy terry material.",
-  //     image:
-  //       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/624007/60/mod01/fnd/PNA/fmt/png/MMQ-Men's-Sweatpants",
-  //     alt: "Men's Pants MMQ",
-  //   },
-  //   {
-  //     key: 10,
-  //     label: "Outfitters Clothes",
-  //     name: "Outfitters",
-  //     desc: "Rev up your style game with this faux leather biker jacket, crafted from luxe PU material. Show off your edgy side with this bold and stylish piece that perfectly combines fashion and function. An essential addition to any wardrobe. (Vroom vroom!)",
-  //     image:
-  //       "https://outfitters.com.pk/cdn/shop/files/F0188101801_2_copy.jpg?v=1738134368",
-  //     alt: "Faux Leather Biker Jacket",
-  //   },
-  // ];
 
   // This function is used to add the items in the cart as an array.
   function btnClick(params) {
@@ -168,9 +75,9 @@ function Clothes({
               overflow: "hidden",
             }}
             className="card"
-            // cover={
-            //   <img src={item.image} alt={item.alt} style={{ height: 250 }} />
-            // }
+            cover={
+              <img src={item.image} alt={item.alt} style={{ height: 250 }} />
+            }
             data={data}
           >
             <PlusCircleFilled
--- form/src/components/shoes/Shoes.jsx
+++ form/src/components/shoes/Shoes.jsx
@@ -17,100 +17,6 @@ function Shoes({
     console.log("Updated Product Data: ", cartItems);
   }, [cartItems]);
 
-  // const data = [
-  //   {
-  //     key: 1,
-  //     label: "Nike Shoes",
-  //     name: "Nike",
-  //     desc: "Start your little one's sneaker collection off with an icon. Designed for basketball but adopted by skaters, the Nike Dunk Low helped define sneakerhead culture. With plush ankle padding and durable rubber traction, this '80s icon is an easy win for playtime and beyond.",
-  //     image:
-  //       "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/83d0c6f9-2176-48ac-8aa1-343e61dea397/NIKE+DUNK+LOW+%28PSE%29.png",
-  //     alt: "Nike Dunk Low",
-  //     logo: "https://www.citypng.com/public/uploads/preview/hd-white-nike-logo-transparent-png-701751694777170hrodp6c2ek.png",
-  //   },
-  //   {
-  //     key: 2,
-  //     label: "Adidas Shoes",
-  //     name: "Adidas",
-  //     desc: "The time-honored silhouette gets an update in the Stan Smith Lux. With a clean and genderless look that nods to the OG, this iteration is effortlessly elevated with premium details. The court-inspired shoe is crafted entirely from buttery-soft leather.",
-  //     image:
-  //       "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/f6bfb2c064a64c498e57af1700593332_9366/Stan_Smith_Lux_Shoes_White_HQ6785_HM1.jpg",
-  //     alt: "Stan Smith Lux Shoes",
-  //   },
-  //   {
-  //     key: 3,
-  //     label: "Puma Shoes",
-  //     name: "Puma",
-  //     desc: "Take to the streets in retro style. These football-inspired kicks deliver superior cushioning for all-day comfort. With PUMA branding and a geometric outsole, make a statement and turn heads. Step into the Turino II, lace up and get ready to score.",
-  //     image:
-  //       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/397452/04/fnd/PNA/fmt/png/PUMA-Turino-II-Men's-Sneakers",
-  //     alt: "PUMA Turino II",
-  //   },
-  //   {
-  //     key: 4,
-  //     label: "Outfitters Shoes",
-  //     name: "Outfitters",
-  //     desc: "Step up your sneaker game with our Multi-piece Sneakers! These kicks feature a unique design made up of multiple pieces, adding an extra twist to your style. Stand out from the crowd with these playful and trendy kicks that are sure to turn heads.",
-  //     image:
-  //       "https://outfitters.com.pk/cdn/shop/files/F0270112002_11.jpg?v=1741062940",
-  //     alt: "Multi-piece Sneakers",
-  //   },
-  //   {
-  //     key: 5,
-  //     label: "Nike Shoes",
-  //     name: "Nike",
-  //     desc: "Building on the design of the Air Foamposite One, this Air Flightposite features the iconic liquid-like, molded upper in a smooth colorway that balances Sail and black with Metallic Silver accents. It pairs a stretchy inner sleeve, create a contained yet comfortable feel.",
-  //     image:
-  //       "https://static.nike.com/a/images/w_1280,q_auto,f_auto/829cb351-c76f-4e57-a160-bdcef3d6f8af/air-flightposite-sail-and-black-hj4481-100-release-date.jpg",
-  //     alt: "Air Flightposite",
-  //   },
-  //   {
-  //     key: 6,
-  //     label: "Nike Shoes",
-  //     name: "Nike",
-  //     desc: "More Air, less bulk. The Dn8 takes our Dynamic Air system and condenses it into a sleek, low-profile package. Powered by eight pressurized Air tubes, it gives you a responsive sensation with every step. Enter an unreal experience of movement.",
-  //     image:
-  //       "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/76442ca5-ae5f-43fc-962a-0c69016e8dd8/AIR+MAX+DN8.png",
-  //     alt: "Nike Air Max Dn8",
-  //   },
-  //   {
-  //     key: 7,
-  //     label: "Adidas Shoes",
-  //     name: "Adidas",
-  //     desc: "Although they made their debut on the hardwood, the adidas Campus shoes were quickly adopted just about everywhere else. With this pair, we move the iconic silhouette in another direction and add modern materials, colours and proportions.",
-  //     image:
-  //       "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/4573512cd701491ebd9b51b8e57c5701_9366/Campus_00s_Shoes_Grey_JH8793_15_hover_standard.jpg",
-  //     alt: "Campus 00s Shoes",
-  //   },
-  //   {
-  //     key: 8,
-  //     label: "Adidas Shoes",
-  //     name: "Adidas",
-  //     desc: "You signed up for your first race. Now it's time to train. As you build consistency, speed and endurance, these adidas running shoes support you — one run at a time. They're light and stable with a breezy mesh upper LIGHTMOTION midsole that responds to your stride.",
-  //     image:
-  //       "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/c8a00e745096463984e1c489a7d99948_9366/Duramo_SL_2_Running_Shoes_Black_IH8218_HM3_hover.jpg",
-  //     alt: "Duramo SL 2 Running Shoes",
-  //   },
-  //   {
-  //     key: 9,
-  //     label: "Puma Shoes",
-  //     name: "Puma",
-  //     desc: "Sport-inspired shoes for life on the go. The PUMA Dribble sneakers blend sleek style and everyday comfort so you can focus on the important things. Regular fit Rubber outsole Lace closure PUMA branding details. Color: PUMA White-PUMA Black-Shadow Gray",
-  //     image:
-  //       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_750,h_750/global/400326/04/sv04/fnd/PNA/fmt/png/PUMA-Dribble-Sneakers",
-  //     alt: "Dribble",
-  //   },
-  //   {
-  //     key: 10,
-  //     label: "Outfitters Shoes",
-  //     name: "Outfitters",
-  //     desc: "Get ready to step up your sneaker game with sleek style our Multi-piece Sneakers! These lace-up shoes are the perfect combination of style and comfort, giving you the best of both worlds. Stay on-trend and comfortable with our unique design.",
-  //     image:
-  //       "https://outfitters.com.pk/cdn/shop/files/F0267112001_5.jpg?v=1740651844",
-  //     alt: "Multi-piece Sneakers",
-  //   },
-  // ];
-
   // This function is used to add the items in the cart as an array.
   function btnClick(params) {
     const { key, name, desc, label, image, alt } = params;
@@ -170,9 +76,9 @@ function Shoes({
               overflow: "hidden",
             }}
             className="card"
-            // cover={
-            //   <img src={item.image} alt={item.alt} style={{ height: 250 }} />
-            // }
+            cover={
+              <img src={item.image} alt={item.alt} style={{ height: 250 }} />
+            }
           >
             <PlusCircleFilled
               className="add"
